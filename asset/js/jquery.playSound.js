2e35c3df223385b24df65787087f17c312200b08
zulganteng <101861962+zulganteng@users.noreply.github.com>
2022-03-18 14:48:49 +0700

    Add files via upload

---
+++ asset/js/jquery.playSound.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+    $.extend({
+
+        playSound: function () {
+
+            return $(
+
+                '<audio class="sound-player" autoplay="autoplay" style="display:none;">' +
+
+                '<source src="' + arguments[0] + '" />' +
+
+                '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="true"/>' +
+
+                '</audio>'
+
+            ).appendTo('body');
+
+        },
+
+        stopSound: function () {
+
+            $(".sound-player").remove();
+
+        }
+
+    });
+
+})(jQuery);
